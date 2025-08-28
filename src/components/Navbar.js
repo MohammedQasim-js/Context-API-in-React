14d1018df5f6e92748260b5c3d419fe00d68ac49
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-08-28 21:27:02 +0530

    context state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkTheme, lightTheme, darkTheme } = this.context;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+
     return (
-      <nav>
-        <h2 className="ui centered header">Pioneer Developer</h2>
+      <nav
+        style={{
+          background: theme.background,
+          color: theme.text,
+          height: "120px",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Pioneer Developer</h2>
         <div className="ui three buttons">
           <button className="ui button">Overview</button>
           <button className="ui button">Contact</button>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -11,7 +11,7 @@ class ThemeContextProvider extends React.Component {
     },
     darkTheme: {
       text: "#fff",
-      background: "5c5c5c",
+      background: "#5c5c5c",
     },
   };
 
