83e37b880b3718f17d7ddcafaba6c8f30c71f4d5
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-09-17 21:36:14 +0530

    useReducer Hook

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -1,34 +1,36 @@
-import React, { Children, createContext, use, useState } from "react";
+import React, { Children, createContext, use, useReducer } from "react";
+import { todosReducer } from "../reducers/todosReducer";
 
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-  const [todos, setTodos] = useState([
+  const [todos, dispatch] = useReducer(todosReducer, [
     { text: "Plan the Family trip", id: 1 },
     { text: "Go for shopping for dinne", id: 2 },
     { text: "Go for a walk", id: 3 },
   ]);
 
-  const addTodo = (todo) => {
-    setTodos([
-      ...todos,
-      {
-        text: todo,
-        id: Math.random(),
-      },
-    ]);
-  };
+  // //  We will now use reducer functionality instead of this
+  //   const addTodo = (todo) => {
+  //     setTodos([
+  //       ...todos,
+  //       {
+  //         text: todo,
+  //         id: Math.random(),
+  //       },
+  //     ]);
+  //   };
 
-  const removeTodo = (id) => {
-    setTodos(
-      todos.filter((todo) => {
-        return todo.id !== Number(id);
-      })
-    );
-  };
+  //   const removeTodo = (id) => {
+  //     setTodos(
+  //       todos.filter((todo) => {
+  //         return todo.id !== Number(id);
+  //       })
+  //     );
+  //   };
 
   return (
-    <TodoListContext.Provider value={{ todos, addTodo, removeTodo }}>
+    <TodoListContext.Provider value={{ todos, dispatch }}>
       {children}
     </TodoListContext.Provider>
   );
